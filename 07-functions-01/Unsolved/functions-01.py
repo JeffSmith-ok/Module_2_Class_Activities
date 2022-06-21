1f0ba803901bec14a0833113b7cdb1a3dc90c178
JeffreySmith <jmstranslate@gmail.com>
2022-06-21 13:17:15 -0400

    finished the class activities

--- 07-functions-01/Unsolved/functions-01.py
+++ 07-functions-01/Unsolved/functions-01.py
@@ -1,23 +1,38 @@
 # Define a function `having_fun` that prints "Functions are FUN!".
-# YOUR CODE HERE!
+def having_fun():
+    print("Functions are FUN!")
 
 # Define a function `thirty_seven` that prints the sum of 18 and 19.
-# YOUR CODE HERE!
+
+
+def thirty_seven():
+    sum = 18 + 19
+    print(sum)
+
 
 # Call the two functions you've defined so far.
-# YOUR CODE HERE!
-# YOUR CODE HERE!
+having_fun()
+thirty_seven()
 
 # Define a function `hello` that takes in a string parameter and prints the parameter variable.
-# YOUR CODE HERE!
+
+
+def hello(string_param):
+    print(string_param)
+
 
 # Call your `hello` function.
-# YOUR CODE HERE!
+hello("Hello World!")
 
 # Define a function `average` that calculates the average between two parameters and returns the average.
-# YOUR CODE HERE!
+
+
+def average(num_1, num_2):
+    avg_calc = (num_1 + num_2) / 2
+    return avg_calc
+
 
 # Call the `average` function and assign to a variable `calculated_average`.
 # Print your `calculated_average`.
-# YOUR CODE HERE!
-# YOUR CODE HERE!
+calculated_average = average(90, 10)
+print(f"Calculated Average: {calculated_average}")
--- 09-csv-01/Unsolved/csv-01.py
+++ 09-csv-01/Unsolved/csv-01.py
@@ -13,23 +13,23 @@ tallest = []
 
 # Open the csv
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile)
 
     # Iterate through the data and search for the number the user inputted. Remember to skip the header of the CSV.
     csv_header = next(csvreader)
 
-    for row in csvreader:
+    for col in csvreader:
         # Print the name of the Pokemon(identifier) and Pokedex number(species id) at that number. For example, "Pokemon No. 25 - Pikachu".
-        print(f"Pokemon No. {row[0]} - {row[1]}")
+        print(f"Pokemon No. {col[0]} - {col[1]}")
 
     # Iterate through the data and search for Pokemon whose weight is greater than 3000. Append only the Pokemon's name and weight to the 'heaviest' list.
-        if int(row[4]) > 3000:
-            print(row[4])
-            heaviest.append(row[1])
+        if int(col[4]) > 3000:
+            print(col[4])
+            heaviest.append(col[1])
 
     # Iterate through the data and search for Pokemon whose height is greater than 50. Append only the Pokemon's name and height to the 'tallest' list.
-        if int(row[3]) > 50:
-            tallest.append(row[1])
+        if int(col[3]) > 50:
+            tallest.append(col[1])
 
 # Print the list of heaviest and tallest pokemon
 print(f"Heaviest Pokemon: {heaviest}")
